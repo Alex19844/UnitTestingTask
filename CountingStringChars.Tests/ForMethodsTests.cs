a5cc3c0c92a5c079ba1b311e897c73b5205b1e4e
Oleksii Yefremov <alekseyefremov844@gmail.com>
2024-05-04 11:47:35 +0300

    First Commit to GitHub

--- CountingStringChars.Tests/ForMethodsTests.cs
+++ CountingStringChars.Tests/ForMethodsTests.cs
@@ -67,6 +67,166 @@ namespace CountingStringChars.Tests
         }
 
         [Test]
+        public void GetConsecutIdentLatinCount_StrIsNull_ThrowsException()
+        {
+            // Act
+            Assert.Throws<ArgumentNullException>(() => ForMethods.GetConsecutIdentLatinCount(null));
+        }
+
+        [Test]
+        public void EmptyString_ReturnsZero()
+        {
+            // Arrange
+            string str = string.Empty;
+
+            // Act
+            int result = ForMethods.GetConsecutIdentLatinCount(str);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void SingleCharacterString_ReturnsOne()
+        {
+            // Arrange
+            string str = "a";
+
+            // Act
+            int result = ForMethods.GetConsecutIdentLatinCount(str);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void AllUniqueCharacters_ReturnsOne()
+        {
+            // Arrange
+            string str = "abcde";
+
+            // Act
+            int result = ForMethods.GetConsecutIdentLatinCount(str);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestCase("aabbaaa", ExpectedResult = 3)]
+        [TestCase("bbb", ExpectedResult = 3)]
+        [TestCase("nfddvtr", ExpectedResult = 2)]
+        [TestCase("aabbccceecc", ExpectedResult = 3)]
+        [TestCase("sssstyssrty", ExpectedResult = 4)]
+        [TestCase("aaaa1ccc", ExpectedResult = 4)]
+        [TestCase("aaaa1cccccc", ExpectedResult = 6)]
+        [TestCase("aa1ffff8ccc", ExpectedResult = 4)]
+        [TestCase("a1", ExpectedResult = 1)]
+        public int ConsecutiveIdenticalCharacters_ReturnsCount(string str)
+        {
+            // Act
+            return ForMethods.GetConsecutIdentLatinCount(str);
+        }
+
+        [Test]
+        public void CaseSensitive_ReturnsCount()
+        {
+            // Arrange (intentionally ignoring case sensitivity)
+            string str = "aAbB";
+
+            // Act
+            int result = ForMethods.GetConsecutIdentLatinCount(str);
+
+            // Assert
+            Assert.AreEqual(2, result); // Should find the sequence "bB" (ignoring case)
+        }
+
+        [Test]
+        public void NonLatinCharacters_ReturnsZero()
+        {
+            // Arrange
+            string str = "123#";
+
+            // Act
+            int result = ForMethods.GetConsecutIdentLatinCount(str);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        public void GetConsecutIdentDigitsCount_StrIsNull_ThrowsException()
+        {
+            // Act
+            Assert.Throws<ArgumentNullException>(() => ForMethods.GetConsecutIdentDigitsCount(null));
+        }
+
+        [Test]
+        public void EmptyStr_ReturnsZero()
+        {
+            // Arrange
+            string str = string.Empty;
+
+            // Act
+            int result = ForMethods.GetConsecutIdentDigitsCount(str);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void SingleDigitString_ReturnsOne()
+        {
+            // Arrange
+            string str = "1";
+
+            // Act
+            int result = ForMethods.GetConsecutIdentDigitsCount(str);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void AllUniqueDigits_ReturnsOne()
+        {
+            // Arrange
+            string str = "12345";
+
+            // Act
+            int result = ForMethods.GetConsecutIdentDigitsCount(str);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestCase("1122111", ExpectedResult = 3)]
+        [TestCase("222", ExpectedResult = 3)]
+        [TestCase("3455867", ExpectedResult = 2)]
+        [TestCase("11223334455", ExpectedResult = 3)]
+        [TestCase("44445677894", ExpectedResult = 4)]
+        [TestCase("5555c111", ExpectedResult = 4)]
+        [TestCase("55555t666666", ExpectedResult = 6)]
+        [TestCase("66t4444b333", ExpectedResult = 4)]
+        [TestCase("a1", ExpectedResult = 1)]
+        public int ConsecutiveIdenticalDigits_ReturnsCount(string str)
+        {
+            // Act
+            return ForMethods.GetConsecutIdentDigitsCount(str);
+        }
+
+        [Test]
+        public void NonDigits_ReturnsZero()
+        {
+            // Arrange
+            string str = "abcf=";
+
+            // Act
+            int result = ForMethods.GetConsecutIdentDigitsCount(str);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
         public void GetUpperCharCountRecursive_StrIsNull_ThrowsException()
         {
             // Act
--- CountingStringChars/ForMethods.cs
+++ CountingStringChars/ForMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 
 namespace CountingStringChars
 {
@@ -50,6 +51,126 @@ namespace CountingStringChars
 
             return count;
         }
+
+        /// <summary>
+        /// Returns maximum number of consecutive identical letters of the Latin alphabet in a line.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>Maximum number of consecutive identical letters of the Latin alphabet in a line.</returns>
+        public static int GetConsecutIdentLatinCount(string? str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (str.Length == 0)
+            {
+                return 0;
+            }
+
+            if (str.Length == 1 && IsBasicLetter(str[0]))
+            {
+                return 1;
+            }
+
+            int maxCount = 0;
+            int currentCount = 1;
+            bool isLetter = false;
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (IsBasicLetter(str[i]) && IsBasicLetter(str[i - 1]))
+                {
+                    if (char.ToLower(str[i]) == char.ToLower(str[i - 1]))
+                    {
+                        currentCount++;
+                    }
+                    else
+                    {
+                        maxCount = Math.Max(maxCount, currentCount);
+                        currentCount = 1;
+                    }
+                }
+                else
+                {
+                    maxCount = Math.Max(maxCount, currentCount);
+                    currentCount = 1;
+                }
+
+                if (IsBasicLetter(str[i]) || IsBasicLetter(str[i - 1]))
+                {
+                    isLetter = true;
+                }
+            }
+
+            maxCount = Math.Max(maxCount, currentCount);
+            if (!isLetter)
+            {
+                maxCount = 0;
+            }
+
+            return maxCount;
+        }
+
+        public static bool IsBasicLetter(char c)
+        {
+            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+        }
+
+        /// <summary>
+        /// Returns maximum number of consecutive identical digits in a line.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>Maximum number of consecutive identical digits in a line.</returns>
+        public static int GetConsecutIdentDigitsCount(string? str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (str.Length == 0)
+            {
+                return 0;
+            }
+
+            if (str.Length == 1 && char.IsDigit(str[0]))
+            {
+                return 1;
+            }
+
+            int currentCount = 1;
+            int maxCount = 0;
+            bool isDigit = false;
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]) && str[i] == str[i - 1])
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    maxCount = Math.Max(maxCount, currentCount);
+                    currentCount = char.IsDigit(str[i]) ? 1 : 0;
+                }
+
+                if (char.IsDigit(str[i]) || char.IsDigit(str[i - 1]))
+                {
+                    isDigit = true;
+                }
+            }
+
+            maxCount = Math.Max(maxCount, currentCount);
+            if (!isDigit)
+            {
+                maxCount = 0;
+            }
+
+            return maxCount;
+        }
+
 
         /// <summary>
         /// Returns a number of characters in a string.
